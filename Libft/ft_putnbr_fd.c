240c7d031b4c6de9638067d7d57c2735d6c2f668
Diogo Martins Cruz Tavares <diogmart@c1r11s2.42porto.com>
2022-11-08 14:29:53 +0000

    small change

--- Libft/ft_putnbr_fd.c
+++ Libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: diogmart <diogmart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 14:20:10 by diogmart          #+#    #+#             */
-/*   Updated: 2022/11/07 14:24:47 by diogmart         ###   ########.fr       */
+/*   Updated: 2022/11/08 14:27:37 by diogmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,25 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	ft_putstr_fd(ft_itoa(n), fd);
+	if (n == -2147483648)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putchar_fd('2', fd);
+		n = 147483648;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n *= -1;
+	}
+	if (n < 10)
+	{
+		ft_putchar_fd(n + 48, fd);
+		return ;
+	}
+	else
+		ft_putnbr_fd(n / 10, fd);
+	ft_putnbr_fd(n % 10, fd);
 }
 
 /*
