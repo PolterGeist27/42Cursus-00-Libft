9f66d648fa5361b954dc4dea0588be884c94db6c
Diogo Martins Cruz Tavares <diogmart@c1r11s2.42porto.com>
2022-11-10 14:13:42 +0000

    fix small mistake

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: diogmart <diogmart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 16:26:14 by diogmart          #+#    #+#             */
-/*   Updated: 2022/11/10 11:49:36 by diogmart         ###   ########.fr       */
+/*   Updated: 2022/11/10 14:10:17 by diogmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,13 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	if (dest > src)
 	{
 		n--;
-		while (n >= 0)
+		while (n > 0)
 		{
 			*((unsigned char *)dest + n) = *((unsigned char *)src + n);
-			if (n == 0)
-				break ;
 			n--;
 		}
+		if (n == 0)
+			*((unsigned char *)dest + n) = *((unsigned char *)src + n);
 	}
 	else
 	{
